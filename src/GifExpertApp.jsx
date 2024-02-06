ec72f6010112820e2f78383a35f8ec6ba88c0b7e
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2024-02-06 11:00:17 -0300

    otra opcion de cambiar el estado del componente padre

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -6,25 +6,39 @@ export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['One Punch']);
 
-  const onAddCategory = () =>{
-    setCategories(['Elment Agregado', ...categories])
+  // eslint-disable-next-line no-unused-vars
+  const onAddCategory = (newCategory) =>{
+    
+    const repeat = categories.find(element=> element.toLowerCase() === newCategory.toLowerCase());
+    if(repeat){
+      alert('this element exist');
+      return;
+    }
+    // if(categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
   }
-  console.log(categories)
   return (
     <>
       {/*  Titulo */}
       <h1>GifExpertApp</h1>
 
       {/* Input */}
-      <AddCategories setCategories={setCategories} />
+      <AddCategories 
+        onNewCategory={ event => onAddCategory(event)} 
+      />
 
       {/* Listado de Gif */}
       
       {/* <button onClick={ onAddCategory }> Agregar </button> */}
 
       <ol>
-        { categories.map( (element, index) =>{
-          return <li key={index}> {element} </li>;
+        { categories.map( (category) =>{
+          return (
+            <div key={category}>
+              <h3>{ category}</h3>
+              <li> {category} </li>);
+            </div>
         }) }
       </ol>
     </>
--- src/components/AddCategories.jsx
+++ src/components/AddCategories.jsx
@@ -1,21 +1,21 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
 
 
-export const AddCategories = ({ setCategories }) => {
+export const AddCategories = ({ onNewCategory }) => {
 
   const [inputValue, setimputValue] = useState('')
 
   const onInputChange = (event)=>{
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setimputValue(event.target.value)
   }
   const onSubmit = (event) => {
     event.preventDefault();
-    setCategories( categories => [ inputValue, ...categories]);
-    console.log(inputValue);
+    if (inputValue.trim().length <= 1) return
+    onNewCategory( inputValue.trim());
+    setimputValue('')
   }
-
-
   
   return (
     <form onSubmit={ onSubmit }>
