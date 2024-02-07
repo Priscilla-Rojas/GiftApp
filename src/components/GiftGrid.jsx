217ac671a8c6b29d25403c7aae564d16c881f78e
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2024-02-07 09:44:41 -0300

    useState

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,13 +1,20 @@
+import { useEffect, useState } from "react";
 import { getGift } from "../helpers/getGifts";
 
+// eslint-disable-next-line react/prop-types
 export const GiftGrid = ({ category}) => {
   
-  getGift(category);
+  const [counter, setCounter] = useState(10)
+  useEffect( ()=>{
+    getGift(category);
+  }, [])
+
 
   return (
     <>
       <h3>{ category }</h3>
-      <p>Hola Mundo</p>
+      <h5>{ counter}</h5>
+      <button onClick={ ()=>setCounter(counter+1)}> +1</button>
     </>
   )
 }
--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -1,5 +1,5 @@
 export const getGift = async(category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=ufJCD5ldr5d6NNCJtWAHHdiTUaqpbyeq&q=${ category }&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=ufJCD5ldr5d6NNCJtWAHHdiTUaqpbyeq&q=${ category }&limit=10`;
   const resp = await fetch( url );
   const { data } = await resp.json();
 
@@ -8,6 +8,7 @@ export const getGift = async(category) => {
     title: img.title,
     url: img.images.downsized_medium.url
   }))
+
   console.log(gifts)
   return gifts;
 }
