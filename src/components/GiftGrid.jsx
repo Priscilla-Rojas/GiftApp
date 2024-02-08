a74af6ff224cbdc4d05306e86bf529b7f83c4797
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2024-02-08 11:26:49 -0300

    custom hooks

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from "react";
-import { getGift } from "../helpers/getGifts";
+import { GiftItem } from "./GiftItem";
+import { useFetchGifts } from "../hooks/useFetchGifts";
 
-// eslint-disable-next-line react/prop-types
 export const GiftGrid = ({ category}) => {
   
-  const [counter, setCounter] = useState(10)
-  useEffect( ()=>{
-    getGift(category);
-  }, [])
-
+  const { images, isLoading } = useFetchGifts( category); 
 
   return (
     <>
       <h3>{ category }</h3>
-      <h5>{ counter}</h5>
-      <button onClick={ ()=>setCounter(counter+1)}> +1</button>
+      <div className="card-grid">
+      {images.map( ( image )=>(
+        <GiftItem key={ image.id } { ...image } />
+      ))}
+      </div>
     </>
+    
   )
 }
---
+++ src/hooks/useFetchGifts.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import {getGift} from '../helpers/getGifts';
+
+
+
+export const useFetchGifts = (category) => {
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  const getImages = async()=>{
+    const newImg = await getGift(category);
+    setImages(newImg);
+    setIsLoading(false);
+  }
+
+  useEffect( ()=>{
+    getImages()
+  }, [])
+
+  return{
+    images,
+    isLoading,
+  }
+}
