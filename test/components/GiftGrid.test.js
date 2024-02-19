ebdb3d40a91d08e57fd0f167abade0d0ae0fb70f
Priscilla Rojas <73943767+Priscilla-Rojas@users.noreply.github.com>
2024-02-19 16:35:45 -0300

    preparando todo para testear el custom hook

--- test/components/GiftGrid.test.js
+++ test/components/GiftGrid.test.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-undef */
 import { render, screen } from "@testing-library/react";
 import { GiftGrid } from "../../src/components/GiftGrid";
+import { useFetchGifts } from "../../src/hooks/useFetchGifts";
+
+jest.mock( '../../src/hooks/useFetchGifts' );
+
 
 describe('Pruevas en GiftGrid', () => {
 
   const category = 'Mariposa';
 
   test('Debe mostar el Loading..', () => { 
+
+    useFetchGifts.mockReturnValue({
+      images: [],
+      isLoading: true,
+    })
 
     render(<GiftGrid category={ category }/>);
     expect( screen.getByText( 'Loading...'))
@@ -16,7 +25,34 @@ describe('Pruevas en GiftGrid', () => {
   })
 
   test('Debe mostrar items cuando se cargan imagenes por medio del useFetchGift', () => {
-    
+
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'Mariposa',
+        url: 'https://localhost/mariposa.jpg'
+      },
+      {
+        id: 'DEF',
+        title: 'Mapa',
+        url: 'https://localhost/mapa.jpg'
+      },
+      {
+        id: 'GHI',
+        title: 'Goku',
+        url: 'https://localhost/goku.jpg'
+      },
+      {
+        id: 'JKL',
+        title: 'Jungla',
+        url: 'https://localhost/jungla.jpg'
+      },
+    ]
+
+    useFetchGifts.mockReturnValue({
+      images: [gifs],
+      isLoading: false,
+    })
   })
 
 })
